cb6c6f80d0551d29f00b697f44bc45872f1c6e66
MariJern <marina.jtest@gmail.com>
2024-02-22 11:19:20 -0600

    Main updated added new line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello Javangry!");
         System.out.println("Hello World!");
+        System.out.println("\uD83E\uDD9D");
     }
 }
