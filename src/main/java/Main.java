7a47e096bbf09fcf37d4fab1c2979ed975c5d3ea
Alice <aliceyanchevskaya@gmail.com>
2024-02-23 21:57:02 +0300

    Print info about my attempts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("\uD83E\uDD9D");
         System.out.println("It works!");
         System.out.println("Hello Team");
+        System.out.println("I need to try it too");
     }
 }
