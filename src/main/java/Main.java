57ea3c7b0b54f79cebff615cd6a6008afe6a595e
Alice <aliceyanchevskaya@gmail.com>
2024-02-28 21:07:04 +0300

    Print one more message for people

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,7 @@ public class Main {
         System.out.println("Пусть у всех получится выучить java");
         System.out.println("hello_everyone!");
         System.out.println("It's difficult to understand what to do");
+
+        System.out.println("Something else");
     }
 }
