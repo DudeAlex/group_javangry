3b66b5cbd6f9143ec3d5add120aeb6584aec0f12
MariJern <marina.jtest@gmail.com>
2024-03-06 09:44:31 -0600

    Added a new package mariJern to package java

---
+++ src/main/java/mariJern/Main.java
@@ -0,0 +1,12 @@
+package mariJern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("""
+                In every life we have some trouble
+                But when you worry you make it double
+                Don't worry
+                Be happy =)""");
+    }
+}
