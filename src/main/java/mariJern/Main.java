ba39e9577bca7c2058a48aa50c14fc3a585102f6
Marina J <marina.jtest@gmail.com>
2024-03-16 07:23:52 -0500

    Tests Added: NormalizeTextTest,  UpperCaseStringTest (#21)
    * Refactored pom, added junit dependency 4.13.2
    
    * added new tests

--- src/main/java/mariJern/Main.java
+++ src/main/java/mariJern/Main.java
@@ -1,12 +1,27 @@
 package mariJern;
 
-public class Main {
-    public static void main(String[] args) {
+import java.text.Normalizer;
 
-        System.out.println("""
+import static Utils.CustomStringFormat.getUpperCaseString;
+
+public class Main {
+
+    public static String happy = """
                 In every life we have some trouble
                 But when you worry you make it double
                 Don't worry
-                Be happy =)""");
+                Be happy =)""";
+
+    public static String normalizeText(String str) {
+        return Normalizer.normalize(str, Normalizer.Form.NFD)
+                .replaceAll("[^\\p{ASCII}]", "");
+    }
+    public static void main(String[] args) {
+
+
+
+        System.out.println(getUpperCaseString(happy));
+
+
     }
 }
---
+++ src/test/java/mariJern/NormalizeTextTest.java
@@ -0,0 +1,14 @@
+package mariJern;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static mariJern.Main.normalizeText;
+
+public class NormalizeTextTest {
+
+
+    @Test
+    public void test() {
+        Assert.assertEquals("Martins Engelis", normalizeText("Mārtiņš Eņģelis"));
+    }
+}
