73b917b578a49745f8bddae8dbb9a37e22ac8da2
KDrops <knihadrops@gmail.com>
2024-03-25 18:38:00 +0100

    Ls first main

--- src/main/java/kdpropsLinaKa/Main.java
+++ src/main/java/kdpropsLinaKa/Main.java
@@ -3,5 +3,6 @@ package kdpropsLinaKa;
 public class Main {
     public static void main(String[] args) {
         System.out.println("new проект");
+        System.out.println("new проект");
     }
 }
