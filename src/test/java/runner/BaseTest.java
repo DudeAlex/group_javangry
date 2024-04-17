88c7e1fd8761b4426b5c41968c1ca375ddf53684
G.Makeev <ivejean75@gmail.com>
2024-04-17 18:11:07 +0300

    BaseTest abstract class has been added.

---
+++ src/test/java/runner/BaseTest.java
@@ -0,0 +1,48 @@
+package runner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+
+public abstract class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    protected void beforeMethod() {
+        try {
+            startDriver();
+        } catch (Exception e) {
+            closeDriver();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterMethod
+    protected void afterMethod() {
+        closeDriver();
+    }
+
+    private void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+
+            driver = null;
+        }
+    }
+
+    private void startDriver() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        chromeOptions.addArguments("disable-popup-blocking");
+        chromeOptions.addArguments("disable-infobars");
+
+        chromeOptions.addExtensions(new File("./Extentions/GIGHMMPIOBKLFEPJOCNAMGKKBIGLIDOM_5_21_0_0.crx"));
+        driver = new ChromeDriver(chromeOptions);
+    }
+
+}
